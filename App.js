98b7a99a7fd36d9419ec8eaee01914162f6fd943
Hynnot <hynnot@gmail.com>
2019-03-06 13:32:46 +0100

    Save in local storage phone number.

--- App.js
+++ App.js
@@ -1,14 +1,55 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Input } from 'react-native-elements';
+import { AsyncStorage, StyleSheet, Text, View } from 'react-native';
+import { Input, Button, Header } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {phone:''}
+  }
+
+  componentDidMount() {
+    this._retrievePhone()
+  }
+
+  async _retrievePhone() {
+    const phone = await AsyncStorage.getItem('phone')
+    if (null !== phone) {
+      this.setState({phone: phone})
+    }
+  }
+
+  setPhone = (value) => {
+    AsyncStorage.setItem('phone', value);
+    this.setState({phone: value});
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View>
+        <Header
+          centerComponent={{ text: 'Drop Alert', style: { color: '#fff', fontSize: 24 } }}
+        />
+
         <Input
-          placeholder='BASIC INPUT'
+          value={this.state.phone}
+          onChangeText = {this.setPhone}
+          placeholder='Teléfono'
+          keyboardType='numeric'
+          errorMessage='Introduce un teléfono correcto'
+          errorStyle={{ color: 'red' }}
+          leftIcon={
+            <Icon
+              name='mobile-phone'
+              size={24}
+              color='black'
+            />
+          }
+        />
+
+        <Button onPress={this._savePhone}
+          title="Guardar"
         />
       </View>
     );
